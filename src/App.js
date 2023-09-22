7a90198043c924c1c83d160e3a49491baf52ef81
sungjaechois <sungjaechoi39@hivelab.co.kr>
2023-09-22 16:00:53 +0900

    11.컴포넌트 성능 최적화 - 일정관리어플에 많은데이터 랜더링하기

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리엑트 기초 알아보기',
-      checked: true
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+// 데이터 생성
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
       checked: false
-    }
-  ]);
+    })
+  }
+  return array;
+}
+
+//createBulkTodos을 함수 형태인 "createBulkTodos()"로 넣어 주게되면
+//reRendering될 떄마다 "createBulkTodos()"가 호출 된다.
+// useState에 넣은 것과 같이 넣게되면 
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4)
 
